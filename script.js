383f4ad7bbf9a78fb391700a9b7c5790696a67a2
RAaz00r <hmidabouhabsy@gmail.com>
2026-05-15 17:13:25 +0100

    JavaScript modif

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const btn = document.getElementById('submit');
 
     if (btn) {
@@ -6,4 +6,26 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Cette fonctionnalité est actuellement en développement. Veuillez réessayer plus tard.');
         });
     }
-});
+});*/
+
+//////////////////////////////////////
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+
+function verifierConnexion() {
+
+    if (email.value !== "" && password.value !== "") {
+        submit.disabled = false;
+    }
+
+    else {
+        submit.disabled = true;
+    }
+}
+
+submit.disabled = true;
+
+email.addEventListener("input", verifierConnexion);
+password.addEventListener("input", verifierConnexion);
